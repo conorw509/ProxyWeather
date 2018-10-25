65b0a4bd767d395d997536a4e68d7caf9c27614e
Conor Ward <conorward509@hotmail.com>
2018-10-25 17:12:32 +0100

    2nd

--- src/main/java/com/mycompany/jerseytutorial/ProxyWeather.java
+++ src/main/java/com/mycompany/jerseytutorial/ProxyWeather.java
@@ -7,24 +7,44 @@ package com.mycompany.jerseytutorial;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam; 
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 /**
 *
 * @author Conor Ward
 */
-@Path("/geocode")
+@Path("/w")
 public class ProxyWeather {
+
 @GET
-@Path("/{param}") 
-public Response GetGeocodeInfo(@PathParam("param") String city) {
-String URL = "http://maps.googleapis.com/maps/api/geocode/json?address="+city;
+@Path("/{city}")
+@Produces (MediaType.APPLICATION_JSON)
+public Response getWeatherData(@PathParam("city") String city) {
+
+String URL = "http://api.openweathermap.org/data/2.5/weather?q=" +city+ "&mode=json&appid=5b63c828fb64fe38943bc6654f1a522c";
 Client c = ClientBuilder.newClient();
 Response r = c.target(URL).request().get();
 return r;
-} 
 }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
